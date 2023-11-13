5e56cc294bb9cb819dd76db9774a26b0cea54350
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-13 17:14:07 +0600

    category added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Header/Banner/Banner";
+import Category from "./Category/Category";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
